fdafef0f42331324a3721bc7234199d9d0825e95
davidyano600 <godsentryan@gmail.com>
2025-09-25 08:54:35 +0100

    mcommit

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ console.log(path.sep)
 const filepath =path.join("/content","/subfolder", "test.txt")
 
 console.log(filepath)
+
+
+const absolute =path.resolve(__dirname,"/content","/subfolder", "test.txt")
+console.log(absolute)
